ba517c3f60495b9e15597c5dae8ff96d3b18efad
Patrykmclaren14 <wierzbowskipatryk5@gmail.com>
2024-04-03 15:02:37 +0200

    feat: Create useApiMutation hook

---
+++ hooks/use-api-mutation.ts
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import { useMutation } from "convex/react";
+
+const useApiMutation = (mutationFunction: any) => {
+  const [pending, setPending] = useState(false);
+  const apiMutation = useMutation(mutationFunction);
+
+  const mutate = (payload: any) => {
+    setPending(true);
+
+    return apiMutation(payload)
+      .then((result) => {
+        return result;
+      })
+      .catch((error) => {
+        throw error;
+      })
+      .finally(() => setPending(false));
+  };
+
+  return {
+    mutate,
+    pending,
+  }
+};
+
+export default useApiMutation;
