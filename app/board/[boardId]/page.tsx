abcbac85aa679add67853637160352c4e2db6894
Patrykmclaren14 <wierzbowskipatryk5@gmail.com>
2024-04-04 20:15:37 +0200

    feat: Implementation Board Page

---
+++ app/board/[boardId]/page.tsx
@@ -0,0 +1,17 @@
+import Canvas from "./_components/canvas";
+
+interface BoardPageProps {
+  params: {
+    boardId: string;
+  };
+};
+
+const BoardPage = ({
+  params,
+}: BoardPageProps) => {
+  return (
+    <Canvas boardId={params.boardId} />
+  );
+};
+
+export default BoardPage;
