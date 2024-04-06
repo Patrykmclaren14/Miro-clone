930d9f8c64ed593e67dc5a1b50a2b77bc5d5e4f3
Patrykmclaren14 <wierzbowskipatryk5@gmail.com>
2024-04-06 14:40:37 +0200

    feat: Implement cursor on canvas

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -1,8 +1,8 @@
 'use client';
 
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
-import { CanvasMode, CanvasState } from "@/types/canvas";
+import { Camera, CanvasMode, CanvasState } from "@/types/canvas";
 
 import Info from "./info";
 import Participants from "./participants";
@@ -10,8 +10,11 @@ import Toolbar from "./toolbar";
 import {
   useHistory,
   useCanUndo,
-  useCanRedo
+  useCanRedo,
+  useMutation,
 } from "@/liveblocks.config";
+import CursorsPresence from "./cursors-presence";
+import { pointerEventToCanvasPoint } from "@/lib/utils";
 
 interface CanvasProps {
   boardId: string;
@@ -22,11 +25,37 @@ const Canvas = ({
 }: CanvasProps) => {
   const [canvasState, setCanvasState] = useState<CanvasState>({
     mode: CanvasMode.None,
-  })
+  });
+
+  const [camera, setCamera] = useState<Camera>({ x: 0, y: 0 });
 
   const history = useHistory();
   const canUndo = useCanUndo();
   const canRedo = useCanRedo();
+
+  const onWheel = useCallback((e: React.WheelEvent) => {
+    setCamera((camera) => ({
+      x: camera.x - e.deltaX,
+      y: camera.y - e.deltaY
+    }));
+  }, []);
+
+  const onPointerMove = useMutation((
+    {setMyPresence }, 
+    e: React.PointerEvent
+  ) => {
+      e.preventDefault();
+
+      const current = pointerEventToCanvasPoint(e, camera);
+
+      setMyPresence({ cursor: current });
+  }, []);
+
+  const onPointerLeave = useMutation((
+    { setMyPresence }
+  ) => {
+    setMyPresence({ cursor: null })
+  }, [])
 
   return (
     <main
@@ -42,6 +71,16 @@ const Canvas = ({
         undo={history.undo}
         redo={history.redo}
       />
+      <svg
+        className="h-[100vh] w-[100vw]"
+        onWheel={onWheel}
+        onPointerMove={onPointerMove}
+        onPointerLeave={onPointerLeave}
+      >
+        <g>
+          <CursorsPresence />
+        </g>
+      </svg>
     </main>
   );
 };
